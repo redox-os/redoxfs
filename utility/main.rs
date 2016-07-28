fb2eb845ba20bc5d7f5b5c4276747dcf801e18bf
Jeremy Soller <jackpot51@gmail.com>
2016-07-28 16:55:36 -0600

    Increase size of image

--- utility/main.rs
+++ utility/main.rs
@@ -219,8 +219,8 @@ fn main() {
                 Err(err) => println!("redoxfs: failed to open image {}: {}", path, err)
             }
         }else{
-            //Create a 128 MB disk image
-            let size = 128 * 1024 * 1024;
+            //Create a 1 GB disk image
+            let size = 1024 * 1024 * 1024;
             match Image::create(&path, size) {
                 Ok(disk) => match FileSystem::create(Box::new(disk)) {
                     Ok(filesystem) => {
