82118c1967c326c6e0b3f9980f2fea0b33c50148
Ibuki.O <isan.ethen@gmail.com>
2025-07-03 18:35:55 +0900

    fix.

--- src/mount/redox/scheme.rs
+++ src/mount/redox/scheme.rs
@@ -190,7 +190,7 @@ impl<'sock, D: Disk> FileScheme<'sock, D> {
     fn handle_connect(&mut self, id: usize, payload: &mut [u8]) -> Result<usize> {
         println!("FileScheme::handle_connect: id {}", id);
         let target_fd = self.other_scheme_fd_map.get(&id).ok_or(Error::new(EBADF))?;
-        let buffer = [0u8; 100];
+        let mut buffer = [0u8; 100];
         let len = syscall::fpath(*target_fd, &mut buffer)?;
         let path = str::from_utf8(&buffer[..len]).map_err(|_| Error::new(EINVAL))?;
         println!(
