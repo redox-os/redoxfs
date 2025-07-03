18ef352dcdc529653927f3ad2e3f19af78e709c4
Ibuki.O <isan.ethen@gmail.com>
2025-07-03 11:47:19 +0900

    fix: Deref target_fd an argument of get_ott_to_socket.

--- src/mount/redox/scheme.rs
+++ src/mount/redox/scheme.rs
@@ -189,7 +189,7 @@ impl<'sock, D: Disk> FileScheme<'sock, D> {
 
     fn handle_connect(&mut self, id: usize, payload: &mut [u8]) -> Result<usize> {
         let target_fd = self.other_scheme_fd_map.get(&id).ok_or(Error::new(EBADF))?;
-        let len = libredox::call::get_ott_to_socket(target_fd, payload)?;
+        let len = libredox::call::get_ott_to_socket(*target_fd, payload)?;
         println!(
             "FileScheme::handle_connect: fd {} -> {}, len {}",
             id, target_fd, len
