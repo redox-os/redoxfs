25119da692a88320f66807e16ac150793a335944
Wildan M <willnode@wellosoft.net>
2026-07-20 23:32:20 +0700

    Use rangemap to fix issues around fmap

--- src/mount/redox/scheme.rs
+++ src/mount/redox/scheme.rs
@@ -914,7 +914,7 @@ impl<'sock, D: Disk> SchemeSync for FileScheme<'sock, D> {
     fn ftruncate(&mut self, id: usize, len: u64, _ctx: &CallerCtx) -> Result<()> {
         // println!("Ftruncate {}, {}", id, len);
         let file = Handle::get_resource_mut(self.handles.get_mut(&id))?;
-        self.fs.tx(|tx| file.truncate(len, tx))
+        self.fs.tx(|tx| file.truncate(&mut self.fmap, len, tx))
     }
 
     fn futimens(&mut self, id: usize, times: &[TimeSpec], _ctx: &CallerCtx) -> Result<()> {
