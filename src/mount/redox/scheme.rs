28ea027818d086dfafd5e6e8f3f6e0b4b506db53
Wildan M <willnode@wellosoft.net>
2026-08-27 07:17:18 +0700

    Adapt relibc symlink resolve

--- src/mount/redox/scheme.rs
+++ src/mount/redox/scheme.rs
@@ -20,7 +20,7 @@ use syscall::error::{
 };
 use syscall::flag::{
     EventFlags, MapFlags, StdFsCallKind, O_ACCMODE, O_CREAT, O_DIRECTORY, O_EXCL, O_NOFOLLOW,
-    O_RDONLY, O_RDWR, O_STAT, O_SYMLINK, O_TRUNC, O_WRONLY,
+    O_RDONLY, O_RDWR, O_SYMLINK, O_TRUNC, O_WRONLY,
 };
 use syscall::schemev2::NewFdFlags;
 use syscall::FobtainFdFlags;
@@ -290,8 +290,10 @@ impl<'sock, D: Disk> FileScheme<'sock, D> {
                         ))
                     }
                 } else if node.data().is_symlink()
-                    && !(flags & O_STAT == O_STAT && flags & O_NOFOLLOW == O_NOFOLLOW)
-                    && flags & O_SYMLINK != O_SYMLINK
+                    // TODO: O_NOFOLLOW & O_RDONLY should throw ELOOP, thus not giving the handle to read symlink
+                    && !(flags & O_NOFOLLOW == O_NOFOLLOW)
+                    // TODO: Old relibc symlink traversal, delete this condition later
+                    && !(flags & O_SYMLINK == O_SYMLINK && flags & O_RDONLY == O_RDONLY)
                 {
                     // Without O_NOFOLLOW, we have to continue traversing symlink
                     let mut resolve_nodes = SmallVec::new();
